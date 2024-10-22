16ebd6f050737567a0dc237a9ac3042a705b0554
Radmila00 <radmilagubajdullina844@gmail.com>
2024-10-22 22:48:42 +0500

    на встроенном мультиплексоре

---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+)
+
+type Transaction struct {
+	ID     int     `json:"id"`
+	Amount float64 `json:"amount"`
+	Type   string  `json:"type"`
+}
+
+type User struct {
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+var (
+	transaction []Transaction
+	nextID      = 1
+	mutex       sync.Mutex //для безопасного доступа нескольких горутин
+)
+
+func main() {
+	//mux := http.NewServeMux()
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/addTransaction", addTransaction)
+	http.HandleFunc("/getTransaction", getTransaction)
+	fmt.Println("Сервер запущен на порту")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+
+}
+func Home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Это главная страница"))
+}
+
+func addTransaction(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Метод не поддерживается", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var t Transaction
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&t); err != nil {
+		http.Error(w, "Неверный формат", http.StatusBadRequest)
+		return
+	}
+	mutex.Lock()
+	t.ID = nextID
+	nextID++
+	transaction = append(transaction, t)
+	mutex.Unlock()
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(t)
+}
+
+func getTransaction(w http.ResponseWriter, r *http.Request) {
+	mutex.Lock()
+	defer mutex.Unlock()
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(transaction)
+}
